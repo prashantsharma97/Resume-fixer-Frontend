914aa56d1dbb41614eb1d4b3e72dd26388fc80f1
prashantsharma97 <prashantatul565@gmail.com>
2024-07-20 00:10:09 +0530

    changes in frontend sssss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import ResumeForm from './components/ResumeForm';
+// import ResumeForm from './components/ResumeForm';
 import './App.css';
 // lots of changes 
 
 const App = () => (
     <div className="app">
         <Header /> 
+
         <ResumeForm />
     </div>
 );
